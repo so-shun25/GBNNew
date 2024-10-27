de2ca977387914169b1f42287508979c970f6b43
Soyoda Shunki <shun112675@icloud.com>
2024-10-27 12:32:08 +0900

    グループ画面

---
+++ GBNNew/GroupGBN.swift
@@ -0,0 +1,18 @@
+//
+//  GroupGBN.swift
+//  GBNNew
+//
+//  Created by 曽世田隼季 on 2024/10/27.
+//
+
+import SwiftUI
+
+struct GroupGBN: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupGBN()
+}
---
+++ GBNNew/GroupTalk.swift
@@ -0,0 +1,18 @@
+//
+//  GroupTalk.swift
+//  GBNNew
+//
+//  Created by 曽世田隼季 on 2024/10/27.
+//
+
+import SwiftUI
+
+struct GroupTalk: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupTalk()
+}
